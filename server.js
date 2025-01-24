199edf1c7f302de9749727a21e9b0022c8720d39
MotazMHassis <mo3taz_mahmood@yahoo.com>
2025-01-24 11:11:39 +0200

    adding files

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve the HTML page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Socket.IO for signaling
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    // Handle signaling messages
+    socket.on('signal', (data) => {
+        // Broadcast the signal to the other peer
+        socket.broadcast.emit('signal', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
